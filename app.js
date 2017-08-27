5932d57a824a69156b179a304f86e942b4c0e495
Mario Zamora <maza1387@gmail.com>
2017-08-27 14:49:48 -0700

    Create GET request, Parse data, Capture command line arguments

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-// Problem: We need a simple way to look at a user's badge count and JavaScript points
-// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+const https = require('https');
+
+// funtion to print message to console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points.`;
+  console.log(message);
+}
+
+function getProfile(username) {
+  // connect to the api url
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+
+    let body = "";
+    // read the data
+    response.on('data', data => {
+      body += data.toString();
+    });
+
+    response.on('end', () => {
+      // parse the data
+      const profile = JSON.parse(body);
+      // print the data
+      printMessage(username, profile.badges.length, profile.points.total)
+    })
+  });
+}
+
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
